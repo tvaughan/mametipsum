2d1b08d5a27bbcd417c72d968bdff703b8a05771
Tom Vaughan <tom@software6.net>
2011-09-21 16:34:06 -0300

    perhaps obsolete

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject mametipsum "0.0.1"
   :dependencies
   [[org.clojure/clojure "1.2.1"]
-   [org.clojure/clojure-contrib "1.2.1"]
    [compojure "0.6.5"]
    [ring/ring-jetty-adapter "0.3.9"]])
