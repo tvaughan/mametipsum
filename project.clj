1fea2c778dbad3695be46f3a82d45c32a524d4e0
Tom Vaughan <tom@software6.net>
2011-09-21 00:02:58 -0300

    misc clean-ups per clojure-mode (and paredit)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject mametipsum "0.0.1"
   :dependencies
-    [[org.clojure/clojure "1.2.1"]
-     [ring/ring-jetty-adapter "0.3.9"]])
+  [[org.clojure/clojure "1.2.1"]
+   [ring/ring-jetty-adapter "0.3.9"]])
