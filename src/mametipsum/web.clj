dc0c480139e9f983753fc8b35e473c2842e452b4
Tom Vaughan <tom@software6.net>
2011-09-20 23:04:01 -0300

    print current time. inspired by:
        https://github.com/mmcgrana/ring/blob/master/example/hello_world.clj

--- src/mametipsum/web.clj
+++ src/mametipsum/web.clj
@@ -1,10 +1,13 @@
 (ns mametipsum.web
-  (:use ring.adapter.jetty))
+  (:use ring.adapter.jetty)
+  (:import java.util.Date java.text.SimpleDateFormat))
 
 (defn app [req]
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "Put that coffee down!\n"})
+   :body (str "Put that coffee down! "
+              "The current time is "
+              (.format (SimpleDateFormat. "HH:mm:ss") (Date.)) ".")})
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
