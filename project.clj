00697afbfff2afd3f3f84d60b3a9b69820bf8f82
Tom Vaughan <tom@software6.net>
2011-09-21 16:33:11 -0300

    initial attempt at using compojure

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject mametipsum "0.0.1"
   :dependencies
   [[org.clojure/clojure "1.2.1"]
+   [org.clojure/clojure-contrib "1.2.1"]
+   [compojure "0.6.5"]
    [ring/ring-jetty-adapter "0.3.9"]])
--- src/mametipsum/web.clj
+++ src/mametipsum/web.clj
@@ -1,13 +1,19 @@
 (ns mametipsum.web
   (:use ring.adapter.jetty)
+  (:use compojure.core)
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler])
   (:import java.util.Date java.text.SimpleDateFormat))
 
-(defn app [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body (str "Put that coffee down! "
-              "The current time is "
-              (.format (SimpleDateFormat. "HH:mm:ss") (Date.)) ".")})
+(defroutes mametipsum-routes
+  (GET "/" [] (str
+               "<h2>Put that coffee down!</h2> The current time is "
+               (.format (SimpleDateFormat. "HH:mm:ss") (Date.)) "."))
+  (route/resources "/")
+  (route/not-found "404"))
+
+(def app
+  (handler/site mametipsum-routes))
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
