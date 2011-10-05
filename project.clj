a6f1df4fc9fba539cbafa1be843d6d394a6e6a4f
Tom Vaughan <tom@software6.net>
2011-10-04 23:13:04 -0300

    bump version numbers on dependencies
    also re-order dependencies in ascending order of complexity top to bottom

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mametipsum "0.0.1"
   :dependencies
   [[org.clojure/clojure "1.2.1"]
+   [ring/ring-jetty-adapter "0.3.11"]
    [compojure "0.6.5"]
-   [hiccup "0.3.6"]
-   [ring/ring-jetty-adapter "0.3.9"]])
+   [hiccup "0.3.7"]])
