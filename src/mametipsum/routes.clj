e0525a01622fd736519c85af768abd145b44d47f
Tom Vaughan <tom@software6.net>
2011-11-20 21:24:27 -0300

    form select script titles in database using a drop-down

--- src/mametipsum/routes.clj
+++ src/mametipsum/routes.clj
@@ -6,6 +6,17 @@
    [compojure.core]))
 
 (defroutes main-routes
-  (GET "/" [] (views/home))
+  (GET "/" []
+       (views/home))
+  (GET "/mametipsum" []
+       (views/list-scripts))
+  (POST "/mametipsum/:script" [script]
+        (views/create-script script))
+  (GET "/mametipsum/:script" [script]
+       (views/read-script script))
+  (PUT "/mametipsum/:script" [script]
+       (views/update-script script))
+  (DELETE "/mametipsum/:script" [script]
+          (views/delete-script script))
   (route/resources "/")
   (route/not-found "404"))
--- src/mametipsum/views.clj
+++ src/mametipsum/views.clj
@@ -1,9 +1,11 @@
 (ns mametipsum.views
-  (:import
-   java.util.Date
-   java.text.SimpleDateFormat)
   (:require
-   [hiccup.page-helpers :as page-helpers]))
+   [hiccup.form-helpers :as form-helpers]
+   [hiccup.page-helpers :as page-helpers]
+   [mametipsum.db :as db]))
+
+(defn- list-script-titles []
+  (for [script (db/list-scripts)] (script :title)))
 
 (defn home []
   (page-helpers/html5
@@ -11,8 +13,26 @@
     [:title "mametipsum"]
     (page-helpers/include-css "/css/site.css")]
    [:body
-    [:h1 "Always Be Closing!"]
-    [:p
-     (str
-      "The current time is "
-      (.format (SimpleDateFormat. "HH:mm:ss") (Date.)) ".")]]))
+    (form-helpers/form-to [:get "mametipsum"]
+                          [:p
+                           (form-helpers/drop-down :title (list-script-titles))]
+                          [:p
+                           (form-helpers/label :nwords "Number of Words")
+                           (form-helpers/text-field :nwords)]
+                          [:p
+                           (form-helpers/submit-button "OK")])]))
+
+(defn list-scripts []
+  {:status 501})
+
+(defn create-script [script]
+  {:status 501})
+
+(defn read-script [script]
+  {:status 501})
+
+(defn update-script [script]
+  {:status 501})
+
+(defn delete-script [script]
+  {:status 501})
