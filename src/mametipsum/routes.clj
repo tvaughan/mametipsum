860c234092197c3fe58c0a0641a1a0a18b89b272
Tom Vaughan <tom@software6.net>
2011-12-23 22:52:48 -0300

    a littler reminder about what's what

--- src/mametipsum/routes.clj
+++ src/mametipsum/routes.clj
@@ -8,12 +8,16 @@
 (defroutes main-routes
   (GET "/mametipsum" []
        (views/list-titles))
+  ;; C - Create
   (POST "/mametipsum/:title" [title params]
         (views/create-script title params))
+  ;; R - Read
   (GET "/mametipsum/:title/:nwords" [title nwords]
        (views/read-script title nwords))
+  ;; U - Update
   (PUT "/mametipsum/:title" [title params]
        (views/update-script title params))
+  ;; D - Delete
   (DELETE "/mametipsum/:title" [title]
           (views/delete-script title))
   (route/resources "/")
