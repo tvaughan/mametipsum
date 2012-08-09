a72b9c1379d187b97aee5cf000e1f850f08144e0
Tom Vaughan <tom@software6.net>
2012-08-09 16:48:30 -0700

    update to latest twitter bootstrap, jquery, and clojure

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject mametipsum "0.0.1"
+(defproject mametipsum "0.0.2"
   :dependencies
-  [[org.clojure/clojure "1.2.1"]
+  [[org.clojure/clojure "1.4.0"]
    [ring/ring-jetty-adapter "0.3.11"]
    [ring-middleware-format "0.1.1"]
    [compojure "1.0.0"]])
