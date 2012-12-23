d82676200655bc86babcf1cf83f9718592c5c318
Tom Vaughan <tom@software6.net>
2012-12-23 17:51:22 -0300

    update :path-info when we're running in a .war context

--- src/mametipsum/middleware.clj
+++ src/mametipsum/middleware.clj
@@ -7,8 +7,11 @@
 (defn- wrap-dir-index [handler]
   (fn [req]
     (handler
-     (update-in req [:uri]
-                #(if (= "/" %) "/index.html" %)))))
+     ;; This will update `:path-info` and `:uri` if they both
+     ;; exist. But what we should do is only update `:uri` when
+     ;; `:path-info` does *not* exist.
+     (update-in req (filter #(contains? req %) [:path-info :uri])
+                #(if (re-find #"/$" %) (format "%sindex.html" %) %)))))
 
 (defn api [handler]
   (wrap-restful-params (wrap-restful-response (wrap-dir-index handler))))
