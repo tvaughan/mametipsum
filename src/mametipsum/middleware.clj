73a156e71b2dd938905584280ea4d9bdc4f765af
Tom Vaughan <tom@software6.net>
2012-12-25 00:59:07 -0300

    update :path-info if it exists otherwise update :uri

--- src/mametipsum/middleware.clj
+++ src/mametipsum/middleware.clj
@@ -5,13 +5,12 @@
 
 ;; http://stackoverflow.com/a/7730478/162963
 (defn- wrap-dir-index [handler]
-  (fn [req]
+  (fn [request]
     (handler
-     ;; This will update `:path-info` and `:uri` if they both
-     ;; exist. But what we should do is only update `:uri` when
-     ;; `:path-info` does *not* exist.
-     (update-in req (filter #(contains? req %) [:path-info :uri])
-                #(if (re-find #"/$" %) (format "%sindex.html" %) %)))))
+     (let [k (if (contains? request :path-info) :path-info :uri) v (get request k)]
+       (if (re-find #"/$" v)
+         (assoc request k (format "%sindex.html" v))
+         request)))))
 
 (defn api [handler]
   (wrap-restful-params (wrap-restful-response (wrap-dir-index handler))))
