a6b330514db6d767353d07ca9d4d133511d1fa9c
Tom Vaughan <tom@software6.net>
2013-04-05 12:22:41 -0300

    bump dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject mametipsum "0.0.2"
   :dependencies
-  [[org.clojure/clojure "1.4.0"]
+  [[org.clojure/clojure "1.5.1"]
    [ring/ring-jetty-adapter "1.2.0-SNAPSHOT"]
-   [ring-middleware-format "0.2.3"]
-   [compojure "1.1.3"]]
+   [ring-middleware-format "0.3.0"]
+   [compojure "1.2.0-SNAPSHOT"]]
   :plugins
   [[lein-ring "0.7.5"]]
   :min-lein-version "2.0.0"
