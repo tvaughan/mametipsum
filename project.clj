c0b3f2af7f1c51361c574ff724a93eba5155cd2d
Tom Vaughan <tvaughan@tocino.cl>
2013-11-06 16:05:21 -0300

    bump dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject mametipsum "0.0.2"
   :dependencies
   [[org.clojure/clojure "1.5.1"]
-   [ring/ring-jetty-adapter "1.2.0-SNAPSHOT"]
-   [ring-middleware-format "0.3.0"]
+   [ring/ring-jetty-adapter "1.2.1"]
+   [ring-middleware-format "0.3.1"]
    [compojure "1.2.0-SNAPSHOT"]]
   :plugins
-  [[lein-ring "0.7.5"]]
+  [[lein-ring "0.8.8"]]
   :min-lein-version "2.0.0"
   :ring {:handler mametipsum.core/app})
